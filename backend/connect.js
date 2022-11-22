9dc48301ed5277eb681a0b1e2c78a6aadcd813bb
AtAtanasov2022 <103512175+AtAtanasov2022@users.noreply.github.com>
2022-11-22 18:48:55 +0200

    start working on models

--- backend/connect.js
+++ backend/connect.js
@@ -1,19 +1,36 @@
-let mysql = require("mysql");
-
-let connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "Biology4U",
-});
-
-connection.connect(function (err) {
-  if (err) {
-    return console.error("error: " + err.message);
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+ 'Biology4U',
+ 'root',
+ '',
+  {
+    host: 'localhost',
+    dialect: 'mysql'
   }
+);
 
-  console.log("Connected to the MySQL server.");
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
 });
+
+// let mysql = require("mysql");
+
+// let connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password!1914",
+//   database: "Biology4U",
+// });
+
+// connection.connect(function (err) {
+//   if (err) {
+//     return console.error("error: " + err.message);
+//   }
+
+//   console.log("Connected to the MySQL server.");
+// });
 
 // connection.end(function (err) {
 //   if (err) {
