6ab0d98afbd584d11df4c094fb202daa0f120d03
AtAtanasov2022 <103512175+AtAtanasov2022@users.noreply.github.com>
2022-12-02 21:56:33 +0200

    Start working on the sign in

--- frontend/biology4u/src/views/SignInView.vue
+++ frontend/biology4u/src/views/SignInView.vue
@@ -1,5 +1,42 @@
 <template>
-    <div>
-        
+    <div class="signInView">
+        <div class="inputContainer">
+            <va-input class="mb-4" v-model="username" label="Потребителско име:" placeholder="Потребителско име:" />
+            <va-input class="mb-4" v-model="email" label="Имейл:" placeholder="Имейл:" />
+            <va-input class="mr-4 mb-4" v-model="password" type="password" label="Парола:" placeholder="Парола:"/>
+            <va-input class="mr-4 mb-4" v-model="repeatedPassword" type="password" label="Повторете паролата:" placeholder="Повторете паролата:"/>
+            <va-select class="mb-4" label="Тип на потребителя" :options="options" v-model="userType" />
+            <va-checkbox class="mb-4" v-model="termsAndServices" :label="label" />
+            <va-checkbox class="mb-4" v-model="rememberMe" :label="longLabel" />
+        </div>
     </div>
 </template>
+
+<script>
+
+
+export default {
+    data() {
+        return {
+            username: "",
+            email: "",
+            password: "",
+            repeatedPassword: "",
+            userType: "",
+            termsAndServices: false,
+            rememberMe: false,
+            options: ["учител", "ученик"],
+            label: "Условия",
+            longLabel: "Запомни ме"
+        }
+    }
+}
+</script>
+
+<style scoped>
+.inputContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
