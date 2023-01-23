0c44e38b8cb46781122c68b993e0ce5cfa275cb7
AtAtanasov2022 <103512175+AtAtanasov2022@users.noreply.github.com>
2023-01-23 18:26:39 +0200

    Trying to ignore login and sign up requests

--- frontend/biology4u/src/services/setupInterceptors.js
+++ frontend/biology4u/src/services/setupInterceptors.js
@@ -2,51 +2,55 @@ import axiosInstance from "./api";
 import TokenService from "./token.service";
 
 const setup = (store) => {
-  axiosInstance.interceptors.request.use(
-    (config) => {
-      const token = TokenService.getLocalAccessToken();
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
-
-  axiosInstance.interceptors.response.use(
-    (res) => {
-      return res;
-    },
-    async (err) => {
-      const originalConfig = err.config;
-
-      if (originalConfig.url !== "/users/logIn" && err.response) {
-        // Access Token was expired
-        if (err.response.status === 401 && !originalConfig._retry) {
-          originalConfig._retry = true;
-
-          try {
-            const rs = await axiosInstance.post("/refreshTokens/refresh", {
-              refreshToken: TokenService.getLocalRefreshToken(),
-            });
-
-            const { accessToken } = rs.data;
-
-            store.dispatch('refreshToken', accessToken);
-            TokenService.updateLocalAccessToken(accessToken);
-
-            return axiosInstance(originalConfig);
-          } catch (_error) {
-            return Promise.reject(_error);
-          }
+    axiosInstance.interceptors.request.use(
+        (config) => {
+            if (config.url !== '/logIn' && config.url !== 'signUp') {
+                const token = TokenService.getLocalAccessToken();
+                if (token) {
+                    config.headers.Authorization = `Bearer ${token}`;
+                }
+                return config;
+            }
+        },
+        (error) => {
+            return Promise.reject(error);
         }
-      }
+    );
 
-      return Promise.reject(err);
-    }
-  );
+    axiosInstance.interceptors.response.use(
+        (res) => {
+            return res;
+        },
+        async (err) => {
+            const originalConfig = err.config;
+
+            if (originalConfig.url !== "/logIn" && originalConfig.url !== "/signUp" && err.response) {
+                // Access Token was expired
+                if (err.response.status === 401 && !originalConfig._retry) {
+                    originalConfig._retry = true;
+
+                    try {
+                        const rs = await axiosInstance.post("/refreshTokens/refresh", {
+                            refreshToken: TokenService.getLocalRefreshToken(),
+                        });
+
+                        const { accessToken } = rs.data;
+
+                        store.dispatch('refreshToken', accessToken);
+                        TokenService.updateLocalAccessToken(accessToken);
+
+                        return axiosInstance(originalConfig);
+                    } catch (_error) {
+                        return Promise.reject(_error);
+                    }
+                }
+            }
+
+            return Promise.reject(err);
+        }
+    );
+
+    // setup axios interceptors to all requests except login and sign up in vue 
 };
 
 export default setup;
